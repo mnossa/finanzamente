e5f40e0e1c844edc1e90ce9d73f7a46384848447
Matteo Nossa <ubuntu@ubuntu>
2026-04-14 22:39:50 +0200

    feat: add JSON-LD structured data for articles and breadcrumbs in MagazineController

--- app/Http/Controllers/MagazineController.php
+++ app/Http/Controllers/MagazineController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\MagazineArticle;
 use App\Models\MagazineCategory;
+use Artesaos\SEOTools\Facades\JsonLdMulti;
 use Artesaos\SEOTools\Facades\OpenGraph;
 use Artesaos\SEOTools\Facades\SEOMeta;
 use Artesaos\SEOTools\Facades\TwitterCard;
@@ -104,6 +105,49 @@ class MagazineController extends Controller
         TwitterCard::setTitle($metaTitle);
         TwitterCard::setDescription($metaDescription);
         TwitterCard::setType('summary_large_image');
+
+        // --- Dati strutturati JSON-LD ---
+
+        // 1. Article (richiesto da Google per i rich results)
+        JsonLdMulti::setType('Article');
+        JsonLdMulti::addValue('headline', mb_substr($metaTitle, 0, 110));
+        JsonLdMulti::addValue('description', $metaDescription);
+        JsonLdMulti::addValue('url', route('magazine.show', $article->slug));
+        JsonLdMulti::addValue('datePublished', $article->published_at->toIso8601String());
+        JsonLdMulti::addValue('dateModified', $article->updated_at->toIso8601String());
+        JsonLdMulti::addValue('author', [
+            '@type' => 'Person',
+            'name'  => $article->author_name,
+        ]);
+        JsonLdMulti::addValue('publisher', [
+            '@type' => 'Organization',
+            'name'  => 'Finanzamente',
+            'url'   => config('app.url'),
+            'logo'  => [
+                '@type' => 'ImageObject',
+                'url'   => asset('images/finanzamente-logo.webp'),
+            ],
+        ]);
+        JsonLdMulti::addValue('mainEntityOfPage', [
+            '@type' => 'WebPage',
+            '@id'   => route('magazine.show', $article->slug),
+        ]);
+        if ($article->cover_image_path) {
+            JsonLdMulti::addValue('image', [
+                '@type' => 'ImageObject',
+                'url'   => asset('storage/' . $article->cover_image_path),
+            ]);
+        }
+
+        // 2. BreadcrumbList
+        JsonLdMulti::newJsonLd();
+        JsonLdMulti::setType('BreadcrumbList');
+        JsonLdMulti::addValue('itemListElement', [
+            ['@type' => 'ListItem', 'position' => 1, 'name' => 'Home',                      'item' => route('home')],
+            ['@type' => 'ListItem', 'position' => 2, 'name' => 'Magazine',                  'item' => route('magazine.index')],
+            ['@type' => 'ListItem', 'position' => 3, 'name' => $article->category->name,    'item' => route('magazine.category', $article->category->slug)],
+            ['@type' => 'ListItem', 'position' => 4, 'name' => $article->title,              'item' => route('magazine.show', $article->slug)],
+        ]);
 
         return view('magazine.show', compact('article', 'related'));
     }
